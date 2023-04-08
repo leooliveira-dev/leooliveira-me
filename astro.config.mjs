03ddc9fb3e07e267c12fe87ab5fd62f63dda8e70
Leonardo Oliveira <dev@leooliveira.me>
2023-04-08 19:07:34 -0300

    refactor: integrate react for carousel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vue from "@astrojs/vue";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), vue(), svelte()]
+  integrations: [tailwind(), react()]
 });
