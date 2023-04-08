16a1ad1f2db51dd1b7324741f99e5a00f0aaee36
Leonardo Oliveira <dev@leooliveira.me>
2023-04-08 20:09:37 -0300

    chore: menu item type and formatting

--- src/lib/content/model.ts
+++ src/lib/content/model.ts
@@ -1,9 +1,9 @@
 // TODO: use Zod :)
 
 export type Image = {
-    url: string,
-    fileName: string,
-}
+  url: string;
+  fileName: string;
+};
 
 export type ContactInfo = {
   text: string;
@@ -12,70 +12,74 @@ export type ContactInfo = {
 };
 
 export type Skill = {
-    name: string;
-    icon: string;
-}
+  name: string;
+  icon: string;
+};
 
 export type HeroBanner = {
-    title: string;
-    subtitle: string;
-}
+  title: string;
+  subtitle: string;
+};
 
 export type About = {
-    title: string;
-    content: string;
-    image: Image;
-}
+  title: string;
+  content: string;
+  image: Image;
+};
 
 export type HardSkills = {
-    title: string;
-    subtitle: string;
-    fundamentals: Skill[];
-    css: Skill[];
-    ui: Skill[];
-    metas: Skill[];
-    tools: Skill[];
-}
+  title: string;
+  subtitle: string;
+  fundamentals: Skill[];
+  css: Skill[];
+  ui: Skill[];
+  metas: Skill[];
+  tools: Skill[];
+};
 
 export type SoftSkills = {
-    title: string;
-    skills: Skill[];
-}
+  title: string;
+  skills: Skill[];
+};
 
 export type Project = {
-    preview: Image;
-    title: string;
-    description: string;
-    stackDescription: string;
-    stack: string[];
-    date: string;
-    link: string;
-}
+  preview: Image;
+  title: string;
+  description: string;
+  stackDescription: string;
+  stack: string[];
+  date: string;
+  link: string;
+};
 
 export type Portfolio = {
+  subtitle: string;
+  companies: Image[];
+  cardCTA: string;
+  projects: Project[];
+  repo: {
     subtitle: string;
-    companies: Image[];
-    cardCTA: string;
-    projects: Project[];
-    repo: {
-        subtitle: string;
-        cta: string;
-        link: string;
-    }
-}
+    cta: string;
+    link: string;
+  };
+};
 
 export type Contact = {
-    title: string;
-    subtitle: string;
-    contacts: ContactInfo[];
-}
+  title: string;
+  subtitle: string;
+  contacts: ContactInfo[];
+};
 
 export type HomePage = {
-    heroBanner: HeroBanner;
-    about: About;
-    hardSkills: HardSkills;
-    softSkills: SoftSkills;
-    portfolio: Portfolio;
-    contact: Contact;
-    footer: string;
-}
+  menu: {
+    text: string;
+    link: string;
+  }[];
+  heroBanner: HeroBanner;
+  about: About;
+  hardSkills: HardSkills;
+  softSkills: SoftSkills;
+  portfolio: Portfolio;
+  contact: Contact;
+  footer: string;
+};
